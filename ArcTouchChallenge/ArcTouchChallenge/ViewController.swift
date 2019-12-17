c8904f1ffbb33660785b937ca63b347a59086d00
levycristian <jolevy.cristian@gmail.com>
2019-12-17 12:05:10 -0300

    ViewCode setup

---
+++ ArcTouchChallenge/ArcTouchChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArcTouchChallenge
+//
+//  Created by Levy Cristian on 17/12/19.
+//  Copyright © 2019 Levy Cristian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
