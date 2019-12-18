d00f1309a1ea7afeb5a643737bde348f09a01886
levycristian <jolevy.cristian@gmail.com>
2019-12-18 16:23:11 -0300

    TimerManager created

---
+++ ArcTouchChallenge/ArcTouchChallenge/View/QuizView/TimerManager.swift
@@ -0,0 +1,52 @@
+//
+//  TimerManager.swift
+//  ArcTouchChallenge
+//
+//  Created by Levy Cristian on 18/12/19.
+//  Copyright © 2019 Levy Cristian. All rights reserved.
+//
+
+import UIKit
+
+class TimerManager {
+    
+    typealias UpdateTimerClousure = ((String) -> Void)
+    typealias BooleanClousure = ((Bool) -> Void)
+    
+    var updatedTimerValue: UpdateTimerClousure?
+    var didFinishQuiz: BooleanClousure?
+    
+    var timer: Timer? = nil
+    private var timerStringFormated: String {
+        let minutes = Int(secondsLeftTimer) / 60 % 60
+        let seconds = Int(secondsLeftTimer) % 60
+        return String(format:"%02i:%02i", minutes, seconds)
+    }
+    
+    let totalTimerSeconds: Int = 5 * 60
+    var secondsLeftTimer: Int = 5 * 60
+    
+    
+    
+    func startTimer() {
+       timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [unowned self] (timer) in
+            self.secondsLeftTimer -= 1
+            self.updatedTimerValue?(self.timerStringFormated)
+           
+           if self.secondsLeftTimer <= 0 {
+               self.didFinishQuiz?(false)
+           }
+       }
+    }
+    
+    func invalidateTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func resetCounterAndTimer() {
+        secondsLeftTimer = 5 * 60
+        updatedTimerValue?(timerStringFormated)
+    }
+    
+}
